26490dcbd0b940319aeb2e7654143b22e1b6c699
Matheus <matheuseduardo.jp@gmail.com>
2022-06-24 20:07:33 -0300

    Conteudo do Bloco 8.3

---
+++ 01.Fundamentos/08.HigherOrderFunctions /03.Filter&Map/script.js
@@ -0,0 +1,75 @@
+// Exercícios do conteúdo
+// const sumFixAmount = (amount) => {
+//     return (number) => amount + number;
+//   }
+
+//   const initialSum = sumFixAmount(15)
+//   console.log(initialSum(5));
+//   const repeat = (number, action) => {
+//     for (let count = 0; count <= number; count += 1) {
+//       action(count);
+//     }
+//   };
+
+//   repeat(5, console.log);
+
+
+const listaNomes = ['Pedro Guerra', 'Luiza Drumond', 'Carla Paiva', 'Matheus Eduardo', 'Mayara Furtado', 'Antonio Eduardo', 'Beatriz Furtado', 'Eduarda Sousa']
+const employees = [];
+
+const newUserEmail = (name) => {
+	return ({
+		Nome: name,
+		Email: `${name.toLowerCase().replace(' ', '_')}@trybe.com`,
+	});
+}
+
+const newEmployees = (nomes, callback) => {
+  nomes.forEach((element, index) => {
+    employees[index] = callback(element);
+  });
+  return employees;
+};
+console.log(newEmployees(listaNomes, newUserEmail));
+
+// console.log(employees);
+
+// const employeesAddAges = (listaColaboradores) => {
+//   for (let index = 0; index < listaColaboradores.length; index += 1) {
+//     employees[index].Age = listaColaboradores[index].Nome.replace(' ', ''). length * 3;
+//   }
+//   return employees;
+// }
+
+const employeesAddAges = (lista) => {
+  lista.forEach((element, index) => {
+    employees[index].Age = element.Nome.replace(' ', ''). length * 3;
+  });
+  return employees;
+}
+employeesAddAges(employees);
+console.table(employees);
+
+const youngling = (lista) => {
+  return lista.find(element => element.Age <= 30);
+}
+console.table(youngling(employees));
+
+// ----------------------------------------------------------------------
+
+const older = (lista) => {
+  return lista.filter((element) => element.Age >= 40);
+}
+console.table(older(employees))
+
+const employeesWithCar = (lista) => {
+  lista.map((element) => {
+    if (element.Age > 35) {
+      return element.Car = true;
+    }
+    return element.Car = false;
+  });
+  return lista;
+} 
+
+console.table(employeesWithCar(employees));
