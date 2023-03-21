bafae7c87f1e945e19a729ddb2b72352c151ec64
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-03-21 18:11:28 -0300

    Conteudo: CS - Introdução ao python

---
+++ 04.CienciaDaComputação/01.Python/area.py
@@ -0,0 +1,26 @@
+PI = 3.14  # PI é uma "constante" em nosso módulo
+
+
+def square(side):
+    # Calculate area of square
+    return side * side
+
+
+def rectangle(length, width):
+    # Calculate area of rectangle
+    return length * width
+
+
+def circle(radius):
+    # Calculate area of circle
+    return PI * radius * radius
+
+
+def triangle(base, height):
+    # Calculate area of triangle
+    return base * height / 2
+
+
+''' execução do módulo 
+olha só, um bloco de comentário
+'''
